4d26c6badd962cee2eae32316bf904af01933fb7
Peter Molnar <peter@petermolnar.hu>
2015-10-15 22:26:21 +0200

    Initial commit

---
+++ PlayingWithCoreDataiOS7Stanford/PlayingWithCoreDataiOS7Stanford/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlayingWithCoreDataiOS7Stanford
+//
+//  Created by Peter Molnar on 15/10/2015.
+//  Copyright © 2015 Peter Molnar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
